97eba6b48029255fc9b72c52e2e6a191be3a39a1
mohammed-khalif <mohamed.yusuf@student.moringaschool.com>
2023-06-22 16:03:39 -0400

    complete

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -1,9 +1,8 @@
-
 import requests
 import json
 
 class GetPrograms:
-
+  
   def get_programs(self):
     URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
 
@@ -11,5 +10,17 @@ class GetPrograms:
     return response.content
 
 
-programs = GetPrograms().get_programs()
-print(programs)
+  def program_school(self):
+    programs_list = []
+    programs = json.loads(self.get_programs())
+    for program in programs:
+        programs_list.append(program["agency"])
+
+    return programs_list
+
+
+programs = GetPrograms()
+programs_schools = programs.program_school()
+
+for school in set(programs_schools):
+    print(school)
